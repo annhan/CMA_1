d37deafead0757e9ab406302b6269fb3947feb4a
Riki <annhandt09@gmail.com>
2020-06-10 08:29:52 +0700

    add define Macros

---
+++ Src/CMA_1/aMacros.h
@@ -0,0 +1,4 @@
+//Toán tử ## được sử dụng để nối chuỗi hai token
+//Toán tử # thông báo rằng đoạn văn bản thay thế sẽ được chuyển đổi sang một chuỗi bao quanh bởi dấu ngoặc kép.
+#define CompareAB(a,b)	(((a)==(b)) ? true : false)
+#define FOR(i,a,b)		for (int i = a; i < b; i++)
